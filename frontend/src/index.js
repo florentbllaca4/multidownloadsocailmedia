380481d62ac068e7d637870a5d446f3c7ad17fdd
florentbllaca4 <bllacaflorent31@gmail.com>
2025-04-14 16:55:11 +0200

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,15 @@
-// Importohet React dhe ReactDOM
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'; // Importimi i stilit për aplikacionin tuaj (opsional)
-import App from './App'; // Importimi i komponentës kryesore të aplikacionit
-// ReactDOM.render përdoret për të ngarkuar aplikacionin në DOM
+import './index.css';
+import App from './App';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />  {/* Komponenti kryesor i aplikacionit */}
+    <App />
   </React.StrictMode>,
-  document.getElementById('root') // Ky është vendi ku do të shfaqet aplikacioni (shpesh në 'index.html' në 'public' folder)
+  document.getElementById('root')
 );
 
-// Opsionale: Përdoret për të monitoruar performancën e aplikacionit tuaj
-reportWebVitals();
+// Heqja e këtij importi dhe thirrjeje nëse nuk është e nevojshme:
+// import reportWebVitals from './reportWebVitals';
+// reportWebVitals();
